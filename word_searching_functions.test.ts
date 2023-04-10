395b83000287affcfe6be323c24cb27a3548f02c
William Gadd <wgadd@sasktel.net>
2023-04-10 14:02:24 -0600

    Change function to handle words repeated in the input, case insensitive

--- word_searching_functions.test.ts
+++ word_searching_functions.test.ts
@@ -1,4 +1,4 @@
-import {getVerseWords} from "./word_searching_functions.js";
+import {getVerseWords} from "./word_searching_functions";
 describe(getVerseWords, () => {
   it("outputs a list of the correct length for a sentence with distinct words", () => {
     expect(getVerseWords("This is a sentence.")).toHaveLength(4);
@@ -9,4 +9,18 @@ describe(getVerseWords, () => {
       getVerseWords("This is a good test and a good sentence")
     ).toHaveLength(7);
   });
+
+  it("outputs a list of the correct length for a sentence where some words are repeated with lowercase, then uppercase capitalization", () => {
+    expect(
+      getVerseWords("This is a good test. Is that really true?")
+    ).toHaveLength(8);
+  });
+
+  it("outputs a list of the correct length for a sentence where some words are repeated with uppercase, then lowercase capitalization", () => {
+    expect(
+      getVerseWords("Is this a good test? Or is it a bad one?")
+    ).toHaveLength(9);
+  });
+
+
 });
--- word_searching_functions.ts
+++ word_searching_functions.ts
@@ -1,9 +1,19 @@
 export const getVerseWords: (sentence: string) => string[] = function(sentence) {
-  const wordObj = {};
+  const wordObj :{[word:string]: number} = {};
   const regex = /[a-z]+/gi;
   const wordListWithReps = sentence.match(regex);
   if(!wordListWithReps) {
     throw new Error("No words found in input sentence");
   }
-  return wordListWithReps;
+  for(const word of wordListWithReps) {
+    const lowercaseWord = word.toLowerCase();
+    if(!wordObj[lowercaseWord]) {
+      wordObj[lowercaseWord];
+    }
+
+    wordObj[lowercaseWord]++;
+  }
+
+  const wordListNoReps = Object.keys(wordObj);
+  return wordListNoReps;
 };
