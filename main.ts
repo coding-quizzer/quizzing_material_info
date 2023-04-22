94ca62ac1f54b00fae2a9e5c9c5c9379f15bd4ec
William Gadd <wgadd@sasktel.net>
2023-04-21 22:03:20 -0600

    Create main.ts as a playground to explore the current constructed functions

--- main.ts
+++ main.ts
@@ -1,13 +1,25 @@
+import { fullWordList } from "./word_searching_functions";
 const { addVerseWordsToFullList } = require("./word_searching_functions");
 const jsonData = require("./data/Luke_1_club150.json");
 
 console.log(jsonData);
 
-const fullWordList = {};
+const completeWordList: fullWordList = {};
 
 for(let verse of jsonData) {
-  addVerseWordsToFullList(verse, fullWordList);
+  addVerseWordsToFullList(verse, completeWordList);
 }
 
-console.log(fullWordList);
+const sortedValues = Object.values(completeWordList);
+sortedValues.sort((a, b) => ("" + a.word).localeCompare(b.word));
+
+const filteredValues = Object.values(completeWordList).filter(
+  (wordObj) => wordObj.references.length === 1
+);
+
+console.table(sortedValues);
+
+filteredValues.sort((a, b) => (("" + a.word).localeCompare(b.word)))
+
+console.table(filteredValues);
 
